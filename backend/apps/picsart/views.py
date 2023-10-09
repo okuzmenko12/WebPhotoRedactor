4f3d854305248b6b8e7abbc39cf999e3d2b302db
okuzmenko31 <olegkuzmenko2005@gmail.com>
2023-10-09 21:41:11 +0300

    Finalized limitations of free usage

--- backend/apps/picsart/views.py
+++ backend/apps/picsart/views.py
@@ -31,7 +31,7 @@ class BaseImageAPIView(IPAddressesUsageCountMixin,
 
         format_error = self.psc.validate_image_format(image)
         reach_limit_resp = Response({
-            'error': 'You have reached the usage limit of this feature!'
+            'error': 'You have reached the usage limit of this feature in this month!'
         }, status=status.HTTP_400_BAD_REQUEST)
 
         user: User = self.request.user
@@ -66,26 +66,29 @@ class BaseImageAPIView(IPAddressesUsageCountMixin,
             else:
                 if user_count_of_enhances == 0:
 
-                    # if ip_address is not None:
-                    #     attempts = self.ip_attempts_for_field(
-                    #         ip_address, self.counter_enhance_field
-                    #     )
-                    #     if attempts >= self.get_features_max_value_of_free_usage():
-                    #         return reach_limit_resp
-                    #     self.ip_increase_field_usage_count(ip_address, self.counter_enhance_field)
-
-                    return Response({
+                    error_resp = Response({
                         'error': 'You don\'t have active subscription'
                                  ' and you don\'t have remaining attempts!'
                                  ' To use this feature, subscribe to one of'
                                  ' the plan.'
                     })
-                self.psc.free_version = False
-                decrease_count_of_enhances_for_field(
-                    user,
-                    self.counter_enhance_field,
-                    1
-                )
+
+                    if ip_address is not None:
+                        attempts = self.ip_attempts_for_field(
+                            ip_address, self.counter_enhance_field
+                        )
+                        if attempts >= self.get_features_max_value_of_free_usage():
+                            return error_resp
+                        self.ip_increase_field_usage_count(ip_address, self.counter_enhance_field)
+                    else:
+                        return error_resp
+                else:
+                    self.psc.free_version = False
+                    decrease_count_of_enhances_for_field(
+                        user,
+                        self.counter_enhance_field,
+                        1
+                    )
 
         format_error_response = Response({
             'error': format_error
